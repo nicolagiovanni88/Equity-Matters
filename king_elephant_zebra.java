4eaf20bbf094a9efa5631cdead79b7af6c239c64
nicolagiovanni88 <136751996+nicolagiovanni88@users.noreply.github.com>
2023-06-21 15:23:11 +0700

    Improve error recovery

---
+++ king_elephant_zebra.java
@@ -0,0 +1,84 @@
+public class EquityMatters{
+	
+	public static void main(String[] args){
+		
+//1		
+		int equity = 1; //declare and initialize equity
+
+
+//2 		
+		while(equity <= 1000) { //Loop for 1000 times
+			
+			System.out.println("Equity Matters:" + equity); //Print the equity
+
+//3			
+			equity++; //Increment the Equity 
+			
+		}
+		
+		
+//4		
+		for(int i=0; i<1000; i++){
+			
+			System.out.println("Equity matters : " + (i+1)); //print the equity
+			
+			
+		}
+		
+//5		
+		int j = 0; //declare a integer
+		
+		while(j<1000){
+			
+			System.out.println("Equity matters : " + j); //print the equity
+			
+			j++; //increment the j
+			
+		}
+
+//6		
+		for(int k = 1; k <= 1000; k++) { //Loop for 1000 times
+			
+			System.out.println("Equity matters : " + k); //print the equity
+			
+		}
+		
+//7		
+		for(int l=1; l<=1000; l+=2){ //Loop for 1000 times
+			
+			System.out.println("Equity matters : " + l); //print the equity
+			
+		}
+		
+//8		
+		for(int m=1; m<=1000; m+=3){ //Loop for 1000 times
+			
+			System.out.println("Equity matters : " + m); //print the equity
+			
+		}
+		
+//9		
+		int n = 1; //declare a integer
+		
+		while(n<=1000){
+			
+			System.out.println("Equity matters : " + n); //print the equity
+			
+			n+=2; //increment the n by 2
+			
+		}
+
+//10		
+		int o = 1; //declare a integer
+		
+		while(o<=1000){
+			
+			System.out.println("Equity matters : " + o); //print the equity
+			
+			o+=3; //increment the o by 3
+			
+		}
+
+	}
+	
+}
